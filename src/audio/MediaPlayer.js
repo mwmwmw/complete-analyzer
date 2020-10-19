0795077bcd90c7d98e6b2cf23da73f7aef1cdfaf
Matthew Willox <skintilla@gmail.com>
2020-10-18 23:27:46 -0600

    added start time

--- src/audio/MediaPlayer.js
+++ src/audio/MediaPlayer.js
@@ -5,12 +5,25 @@ export default class MediaPlayer extends AudioBufferSourceNode {
     constructor () {
         super(...arguments);
         this._startTime = 0;
+        console.log(this.buffer)
     }
 
     start () {
-        super.start(arguments);
-        this._startTime(time)
+        super.start(...arguments);
+        this._startTime = arguments[0];
     }
 
+    set buffer (value) {
+        super.buffer = value;
+        this.duration = (value.length/value.sampleRate);
+    }
+
+    get time () {
+        return this.context.currentTime - this._startTime;
+    }
+
+    get position () {
+        return this.time / this.duration;
+    }
 
 }
